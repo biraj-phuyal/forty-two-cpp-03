de92257e2deb30b00730191d4d1aee8d921a15dd
biraj-phuyal <birajphuyal11@gmail.com>
2026-08-25 22:21:18 +0100

    ex00

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -1,17 +1,23 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
 
-
-#include <iostream>
+# include <string>
 
 class ClapTrap {
     private:
         std::string name;
-        int hit_points[10];
-        int energy_points[10];
-        int attack_damage[10];
+        unsigned int hit_points;
+        unsigned int energy_points;
+        unsigned int attack_damage;
     public:
         ClapTrap(void);
+        ClapTrap(const std::string& name);
+        ClapTrap(const ClapTrap& src);
+        ClapTrap& operator=(const ClapTrap& src);
         ~ClapTrap(void);
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
 };
+
+#endif
